2a3a61045a8bc7afc76b5c1b42eace390c35b554
YasserGmr <goumghar.yasser2003@gmail.com>
2022-03-04 12:55:09 +0100

    Aded little thn

--- routes/dataRouter.js
+++ routes/dataRouter.js
@@ -19,81 +19,88 @@ let browser
 router.get('/readLinks', async (req, res, next) => {
   const allProducts = []
 
+  //2 Get all Products number
+  const allProductsDatabase = await Product.find()
+  const i = allProductsDatabase.length
+
   // for (let i = 0; i < productLinks.links.length; i++) {
-  for (let i = 1360; i < productLinks.links.length; i++) {
-    const productLink = productLinks.links[i]
+  // for (let i = 0; i < 10; i++) {
+  const productLink = productLinks.links[i]
 
-    // console.log({ productLink })
+  // console.log({ productLink })
 
-    const newPage = await browser.newPage()
-    await newPage.goto(productLink, {
-      waitUntil: 'networkidle2',
-      timeout: 0,
-    })
+  const newPage = await browser.newPage()
+  await newPage.goto(productLink, {
+    waitUntil: 'networkidle2',
+    timeout: 0,
+  })
 
-    // console.log(`Current Product: ${i + 1}`)
+  // console.log(`Current Product: ${i + 1}`)
 
-    const info = await newPage.evaluate(() => {
-      return {
-        title: document.querySelector('h1[itemprop=name]')?.innerText,
-        preview: document.querySelector('#bigpic').currentSrc,
+  const info = await newPage.evaluate(() => {
+    return {
+      title: document.querySelector('h1[itemprop=name]')?.innerText,
+      preview: document.querySelector('#bigpic').currentSrc,
 
-        images: Array.from(document.querySelectorAll('a.fancybox img')).map(
-          e => e.currentSrc
-        ),
+      images: Array.from(document.querySelectorAll('a.fancybox img')).map(
+        e => e.currentSrc
+      ),
 
-        price: parseFloat(
-          document.querySelector('[itemprop="price"]').attributes.content.value
-        ),
+      price: parseFloat(
+        document.querySelector('[itemprop="price"]').attributes.content.value
+      ),
 
-        reference: document.querySelector('#product_reference span[content]')
+      reference: document.querySelector('#product_reference span[content]')
+        ? document.querySelector('#product_reference span[content]').innerText
           ? document.querySelector('#product_reference span[content]').innerText
-            ? document.querySelector('#product_reference span[content]')
-                .innerText
-            : undefined
-          : undefined,
+          : undefined
+        : undefined,
 
-        description: document.querySelector('.table-data-sheet > tbody')
-          ? Array.from(
-              document.querySelector('.table-data-sheet > tbody').rows
-            ).map(e => {
-              const vartype = Array.from(e.children)[0].outerText
-              const proper = Array.from(e.children)[1].outerText
-              const returnVal = {}
-              returnVal[vartype] = proper
-              return returnVal
-            })
-          : [],
+      description: document.querySelector('.table-data-sheet > tbody')
+        ? Array.from(
+            document.querySelector('.table-data-sheet > tbody').rows
+          ).map(e => {
+            const vartype = Array.from(e.children)[0].outerText
+            const proper = Array.from(e.children)[1].outerText
+            const returnVal = {}
+            returnVal[vartype] = proper
+            return returnVal
+          })
+        : [],
 
-        category: 'manuel',
+      category: 'manuel',
 
-        ficheTechnique: Array.from(
-          document.querySelectorAll('.page-product-box .rte ul li')
-        ).map(e => e?.innerText),
+      ficheTechnique: Array.from(
+        document.querySelectorAll('.page-product-box .rte ul li')
+      ).map(e => e?.innerText),
 
-        presentation: Array.from(
-          document.querySelectorAll('.page-product-box .rte p')
-        ).map(e => e?.innerText),
+      presentation: Array.from(
+        document.querySelectorAll('.page-product-box .rte p')
+      ).map(e => e?.innerText),
 
-        shortDescription: document.querySelector('#short_description_content')
-          ? Array.from(
-              document.querySelector('#short_description_content').children
-            ).map(e => e?.innerText)
-          : [],
-      }
-    })
+      shortDescription: document.querySelector('#short_description_content')
+        ? Array.from(
+            document.querySelector('#short_description_content').children
+          ).map(e => e?.innerText)
+        : [],
+    }
+  })
 
-    await Product.create(info)
-  }
+  await Product.create(info)
+  // }
 
   //2 Add it to DB
   // const productsDb = await Product.insertMany(allProducts)
 
-  res.json({
-    message: 'Hello',
-    length: allProducts.length,
-    // lengthDb: productsDb.length,
-  })
+  if (i === 11807) {
+    res.json({
+      message: 'Hello',
+      // length: allProducts.length,
+      // lengthDb: productsDb.length,
+    })
+  } else {
+    res.redirect(req.originalUrl)
+  }
 })
 
 module.exports = router
